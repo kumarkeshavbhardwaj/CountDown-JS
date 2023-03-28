c5e5711ac91cf9107e04cc0e9852e6ea1c82d514
kumar keshav <kumarkeshavbhardwaj@gmail.com>
2023-03-28 21:38:42 +0530

    Implementing maths

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-const endDate = "27 March 2022 10:00 PM"
+const endDate = "31 March 2023 12:00 PM"
 
 document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll('input');
+
+
+
+clock();
+
+
+function clock () {
+    const end = new Date(endDate);
+    const now = new Date();
+
+
+    const diff = (end - now)/1000;
+    //date is below
+    inputs[0].value = Math.floor(diff/3600/24);
+
+    //hours is below
+    inputs[1].value = Math.floor((diff/3600) % 24);
+
+    //minutes is below
+    inputs[2].value = Math.floor((diff/60) % 60);
+
+    //seconds is below
+    inputs[3].value = Math.floor(diff%60);
+
+
+}
